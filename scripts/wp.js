ddcef23349edf72212ff32efd93b7d1f3b2449ce
Johnson Chandra <johnson.chandra@gmail.com>
2015-06-06 16:47:31 +0700

    initial

---
+++ scripts/wp.js
@@ -0,0 +1,31 @@
+/* Our Script */
+
+//console.log('Hallo - From Our');
+
+$(document).ready(function() {
+
+  $("#owl-example").owlCarousel();
+
+});
+
+
+$(document).ready(function() {
+
+  $("#owl-demo").owlCarousel({
+
+      navigation : true, // Show next and prev buttons
+      slideSpeed : 300,
+      paginationSpeed : 400,
+      singleItem:true
+
+      // "singleItem:true" is a shortcut for:
+      // items : 1, 
+      // itemsDesktop : false,
+      // itemsDesktopSmall : false,
+      // itemsTablet: false,
+      // itemsMobile : false
+
+  });
+
+});
+
